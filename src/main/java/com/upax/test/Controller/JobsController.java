461bb2106330958344441317f8c0297d197630a5
Elielo03 <elielorgz03@gmail.com>
2021-07-17 20:15:08 -0500

    Lista ordenada y filtrada :)

--- src/main/java/com/upax/test/Controller/JobsController.java
+++ src/main/java/com/upax/test/Controller/JobsController.java
@@ -5,8 +5,14 @@
  */
 package com.upax.test.Controller;
 
+import com.upax.test.entity.Employee;
 import com.upax.test.entity.Jobs;
 import com.upax.test.service.IServiceJobs;
+import com.upax.test.service.ISserviceEmployees;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,6 +32,9 @@ public class JobsController {
     @Autowired
     IServiceJobs serviceJobs;
     
+    @Autowired
+    ISserviceEmployees serviceEmployee;
+    
     @PostMapping("/addJob")
     public ResponseEntity<?>addJob(@RequestBody Jobs job){
         serviceJobs.addJob(job);
@@ -36,7 +45,18 @@ public class JobsController {
     public ResponseEntity<?>getJob(@RequestBody Jobs job){
         
         job= serviceJobs.findById(job.getId());
-        return new ResponseEntity<>(job,HttpStatus.OK);
+        List<Employee> list=serviceEmployee.getAll();
+        System.out.println("tam: "+list.size());
+        
+        Long idJob=job.getId();
+
+         List<Employee> listFiltered = list
+        .stream()
+        .filter(c -> Objects.equals(c.getJob_id(), idJob))
+        .collect(Collectors.toList());
+         System.out.println("tam customer: "+listFiltered.size());
+         listFiltered.sort(Comparator.comparing(a -> a.getLastName()));
+        return new ResponseEntity<>(listFiltered,HttpStatus.OK);
     }
     
 }
